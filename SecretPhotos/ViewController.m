ac0bb77fb9b1d63635c32af3ac32e47efdc3609e
duanwuxi <duanwuxi@xxkuaipao.com>
2016-07-13 18:51:13 +0800

    change style

--- SecretPhotos/ViewController.m
+++ SecretPhotos/ViewController.m
@@ -12,10 +12,12 @@
 #import <QuartzCore/CAAnimation.h>
 
 #define bg_image_key @"backgroungImageKey"
+#define button_name_key @"buttonNameKey"
 
 @interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *bgImage;
+@property (weak, nonatomic) IBOutlet UIButton *openButton;
 
 @end
 
@@ -33,13 +35,16 @@
         [_bgImage setImage:im];
     }
     
-    
-    CATransition *myTransition=[CATransition animation];//创建CATransition
-    myTransition.duration=0.3;//持续时长0.3秒
-    myTransition.timingFunction=UIViewAnimationCurveEaseInOut;//计时函数，从头到尾的流畅度
-    myTransition.type=@"reveal";//动画类型
-    myTransition.subtype=kCATransitionFromLeft;//子类型
-    [self.navigationController.view.layer addAnimation:myTransition forKey:nil];
+    NSString *btnTitle = [[NSUserDefaults standardUserDefaults] objectForKey:button_name_key];
+    if(btnTitle){
+        [_openButton setTitle:btnTitle forState:UIControlStateNormal];
+    }
+//    CATransition *myTransition=[CATransition animation];//创建CATransition
+//    myTransition.duration=0.3;//持续时长0.3秒
+//    myTransition.timingFunction=UIViewAnimationCurveEaseInOut;//计时函数，从头到尾的流畅度
+//    myTransition.type=@"reveal";//动画类型
+//    myTransition.subtype=kCATransitionFromLeft;//子类型
+//    [self.navigationController.view.layer addAnimation:myTransition forKey:nil];
 }
 
 - (void) didTapBGImage : (UITapGestureRecognizer *)tap
@@ -109,5 +114,29 @@
     
 }
 
+- (IBAction)disTapSetting:(id)sender {
+    CusTomActionSheetView *sheet = [[CusTomActionSheetView alloc]initWithTitle:NSLocalizedString(@"setting", nil) andItemTitles:@[NSLocalizedString(@"settingButtonName", nil)] cancleTitle:NSLocalizedString(@"cancle", nil)];
+    sheet.handler = ^(NSInteger index){
+        if(index == 0){
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"tip", nil) message:NSLocalizedString(@"inputTip", nil) preferredStyle:UIAlertControllerStyleAlert];
+            [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+               
+                textField.placeholder = NSLocalizedString(@"inputTip", nil);
+                
+            }];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+               
+                UITextField *filed = alert.textFields.firstObject;
+                NSString *titleStr = filed.text;
+                [_openButton setTitle:titleStr forState:UIControlStateNormal];
+                [[NSUserDefaults standardUserDefaults] setObject:titleStr forKey:button_name_key];
+            }];
+            [alert addAction:okAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+        
+    };
+    [sheet show];
+}
 
 @end
